0d2256c84e5b35b6c4e3447429c982acd981c409
Jaden Wu <804876@learn.vsb.bc.ca>
2021-02-11 10:23:31 -0800

    Rock Paper Scissor Assignment

---
+++ Rock Paper Scissor/src/RockPaperScissor.java
@@ -0,0 +1,15 @@
+public class RockPaperScissor {
+    public static void main(String[] args) {
+        //numbers 1,2,3 will represent rock paper scissors
+        int random;
+        System.out.println((int)(Math.random()*3+1));
+
+
+
+
+
+    }
+
+
+
+}
