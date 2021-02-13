e9388df513769379f8994355b19cd629898acf24
jadenw123 <804876@learn.vsb.bc.ca>
2021-02-12 23:47:09 -0800

    Expressions Assignment

--- Expressions Assignment/src/Main.java
+++ Expressions Assignment/src/Main.java
@@ -31,19 +31,29 @@ public class Main {
         //Expression 3
         System.out.println(((8 - 5) + Math.pow(3,4)/5));
         System.out.println(3 + Math.pow(3,4)/5);
-        System.out.println(3 + 81/5);
+        System.out.println(3 + (81 / 5));
         System.out.println(3 + 16.2);
         System.out.println(19.2);
 
         //Expression 4
         System.out.println((9 % 4) - 8 + Math.pow(2,3) / 8 + (Math.sqrt(81) / 3));
-        System.out.println((9 % 4) - 8 + Math.pow(2,3) / 8 + (3));
-        System.out.println((1) - 8 + 8 / 8 + (3));
-        System.out.println((1) - 8 + 1 + 3);
-        System.out.println((-7));
+        System.out.println((9 % 4) - 8 + Math.pow(2,3) / 8 + (3.));
+        System.out.println((1) - 8 + 8 / 8 + (3.));
+        System.out.println((1) - 8 + 1 + 3.);
+        System.out.println((-7 + 1 + 3.));
+        System.out.println((-6 + 3.));
+        System.out.println(-3.);
 
         //Expression 5
         System.out.println(((14 / 2 + 3) * Math.sin(45) + 2) - Math.pow(Math.sqrt(25), 3));
-        System.out.println(((10) * Math.sin(47)) - Math.pow(Math.sqrt(25),3));
+        System.out.println(((7 + 3) * Math.sin(45) + 2) - Math.pow((5.0), 3));
+        System.out.println(((10) * Math.sin(45) + 2) - Math.pow((5.0), 3));
+        System.out.println(((10) * Math.sin(45) + 2) - (125.0));
+        System.out.println(((10) * (0.8509035245341184) + 2) - (125.0));
+        System.out.println(((8.509035245341185) + 2) - (125.0));
+        System.out.println(-114.49096475465882);
+
+
+
     }
 }
