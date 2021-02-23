80323d8ec83e75ad22fb1063aeb20414e6b15cf4
jadenw123 <804876@learn.vsb.bc.ca>
2021-02-22 20:01:49 -0800

    Rock Paper Scissor Assignment

--- Rock Paper Scissor Assignment/src/RockPaperScissor.java
+++ Rock Paper Scissor Assignment/src/RockPaperScissor.java
@@ -13,47 +13,53 @@ public class RockPaperScissor {
 
         String [] plays = {r, p, s};
 
+
+        //player can choose to continue or quit
+        System.out.println("Continue or Quit");
         boolean quit = false;
+
 
         String ComChoice;
         String PlayChoice;
 
         int index;
 
+
         while(!quit){
-            System.out.println("PickRock, Paper, Scissor");
-            PlayChoice = scanner.nextline();
+            System.out.println("Pick Rock, Paper, Scissor");
+            PlayChoice = scanner.next();
 
             index = random.nextInt(3);
             ComChoice = plays[index];
 
+
+            //all the outcomes that can happen
             System.out.println("Player choice:" + PlayChoice +" Computer Choice:" + ComChoice);
 
             if(PlayChoice. equals(ComChoice)){
                 System.out.println("Draw");
 
-            }else if (PlayChoice.equals(r) && ComChoice.equals(p));{
+            }else if (PlayChoice.equals(r) && ComChoice.equals(p)){
 
                 System.out.println("Computer Wins");
 
-            } else if (PlayChoice.equals(r) && ComChoice.equals(s));{
+            } else if (PlayChoice.equals(r) && ComChoice.equals(s)){
 
                 System.out.println("You Win");
 
-            } if(PlayChoice.equals(p) && ComChoice.equals(r));{
+            } else if(PlayChoice.equals(p) && ComChoice.equals(r)){
 
                 System.out.println("You Win");
 
-            } if(PlayChoice.equals(r) && ComChoice.equals(p));{
+            } else if(PlayChoice.equals(p) && ComChoice.equals(s)){
 
                     System.out.println("Computer Wins");
 
-                } if (PlayChoice.equals(s) && ComChoice.equals(r));{
-
+                } else if (PlayChoice.equals(s) && ComChoice.equals(r)){
 
                         System.out.println("Computer Wins");
 
-                    }if (PlayChoice.equals(s) && ComChoice.equals(p));{
+                    }else if (PlayChoice.equals(s) && ComChoice.equals(p)){
 
                         System.out.println("You Win");
 
