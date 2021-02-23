6af83797d2d829d334e3b14c2b38aca0323c7d1a
jadenw123 <804876@learn.vsb.bc.ca>
2021-02-22 21:44:28 -0800

    Rock Paper Scissor Assignment

--- Rock Paper Scissor Assignment/src/RockPaperScissor.java
+++ Rock Paper Scissor Assignment/src/RockPaperScissor.java
@@ -3,73 +3,101 @@ import java.util.Scanner;
 
 public class RockPaperScissor {
     public static void main(String[] args) {
+        //scoreboard
+        int playerwin = 0;
+        int compwin = 0;
 
-        Scanner scanner = new Scanner (System.in);
-        Random random = new Random ();
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
 
+        //player picks rock paper scissor
         String r = "rock";
         String s = "scissor";
         String p = "paper";
 
-        String [] plays = {r, p, s};
+        String[] plays = {r, p, s};
+
+        //player can quit
+        String q = "quit";
 
 
         //player can choose to continue or quit
         boolean quit = false;
 
-
+        //player choice 2 for quiting or keep going
         String ComChoice;
         String PlayChoice;
+        String PlayChoice2;
 
         int index;
 
-
-        while(!quit){
+        //looping game
+        while (!quit) {
             System.out.println("Pick Rock, Paper, Scissor");
             PlayChoice = scanner.next();
 
+            //computer pick 1 2 3 rock paper scissor
             index = random.nextInt(3);
             ComChoice = plays[index];
 
 
             //all the outcomes that can happen
-            System.out.println("Player choice:" + PlayChoice +" Computer Choice:" + ComChoice);
+            System.out.println("Player choice:" + PlayChoice + " Computer Choice:" + ComChoice);
 
-            if(PlayChoice. equals(ComChoice)){
+            if (PlayChoice.equals(ComChoice)) {
                 System.out.println("Draw");
 
-            }else if (PlayChoice.equals(r) && ComChoice.equals(p)){
+            } else if (PlayChoice.equals(r) && ComChoice.equals(p)) {
 
                 System.out.println("Computer Wins");
+                compwin ++;
 
-            } else if (PlayChoice.equals(r) && ComChoice.equals(s)){
-
-                System.out.println("You Win");
-
-            } else if(PlayChoice.equals(p) && ComChoice.equals(r)){
+            } else if (PlayChoice.equals(r) && ComChoice.equals(s)) {
 
                 System.out.println("You Win");
+                playerwin ++;
 
-            } else if(PlayChoice.equals(p) && ComChoice.equals(s)){
+            } else if (PlayChoice.equals(p) && ComChoice.equals(r)) {
 
-                    System.out.println("Computer Wins");
+                System.out.println("You Win");
+                playerwin ++;
 
-                } else if (PlayChoice.equals(s) && ComChoice.equals(r)){
+            } else if (PlayChoice.equals(p) && ComChoice.equals(s)) {
 
-                        System.out.println("Computer Wins");
+                System.out.println("Computer Wins");
+                compwin ++;
 
-                    }else if (PlayChoice.equals(s) && ComChoice.equals(p)){
+            } else if (PlayChoice.equals(s) && ComChoice.equals(r)) {
 
-                        System.out.println("You Win");
+                System.out.println("Computer Wins");
+                compwin ++;
+
+            } else if (PlayChoice.equals(s) && ComChoice.equals(p)) {
+
+                System.out.println("You Win");
+                playerwin ++;
 
             }
-            System.out.println("Continue or Quit");
-            PlayChoice = scanner.next();
+            //scoreboard
+            System.out.println("Your Wins: "+playerwin);
+            System.out.println("Computer Wins: "+compwin);
+
+            //keep playing or quit
+            System.out.println(" Again or Quit");
+            PlayChoice2 = scanner.next();
+            if (PlayChoice2.equals(q)) {
+                quit = true;
+
+            //player cant pick invalid option
+            if (PlayChoice.equals(r p s))
+            System.out.println("Pick Valid Option");
+
+
+            }
 
         }
 
     }
-
 }
 
 
