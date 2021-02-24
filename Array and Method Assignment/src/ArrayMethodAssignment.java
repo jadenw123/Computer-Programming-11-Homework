548cb3b3f545dabc7a4675d901eaf53b197e050c
Jaden Wu <804876@learn.vsb.bc.ca>
2021-02-24 10:41:27 -0800

    ArrayandMethodAssginment

---
+++ Array and Method Assignment/src/ArrayMethodAssignment.java
@@ -0,0 +1,14 @@
+public class ArrayMethodAssignment {
+    public static int[] addNumberToArray(int[] inputArray, int number){
+        int[] outputArray = new int[inputArray.length + 1];
+        return outputArray;
+        for(int i = 0; i<inputArray.length + 1; i++){
+            outputArray[i] = inputArray[i];
+            if inputArray[i] < inputArray.length
+
+        }
+    }
+    public static void main(String[] args) {
+
+    }
+}
