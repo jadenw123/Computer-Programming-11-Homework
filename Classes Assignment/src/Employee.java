459b97f38f0e8b66223e85a47e9ff00969ab9575
jadenw123 <804876@learn.vsb.bc.ca>
2021-03-03 22:13:02 -0800

    Classes Assignment

--- Classes Assignment/src/Employee.java
+++ Classes Assignment/src/Employee.java
@@ -1,12 +1,15 @@
 public class Employee {
 
-        private int id;
-        private String firstName;
-        private String lastName;
+        private final int id;
+        private final String firstName;
+        private final String lastName;
         private int salary;
 
     public Employee(int id, String firstName, String lastName, int salary){
-
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.salary = salary;
     }
 
 
@@ -23,7 +26,7 @@ public class Employee {
     }
 
     public String getName() {
-        return firstName + lastName;
+        return firstName +" "+ lastName;
     }
 
     public int getSalary() {
@@ -33,6 +36,22 @@ public class Employee {
     public void setSalary(int salary) {
         this.salary = salary;
     }
+
+    public int getAnnualSalary(){
+        return salary*12;
+    }
+
+    public int raiseSalary(int percent){
+        return (int)(salary*(double)percent/100);
+    }
+
+    public String toString(){
+        return "id: " + this.id + "\tname: " + getName() + "\tsalary: " + this.salary;
+    }
+
+
+
+
 
 
 }
--- Classes Assignment/src/Main.java
+++ Classes Assignment/src/Main.java
@@ -1,2 +1,14 @@
 public class Main {
+    public static void main(String[] args) {
+        Employee Jef = new Employee(777777, "Jef", "Smith", 100000);
+        System.out.println("id: " + Jef.getId());
+        System.out.println("First Name: " + Jef.getFirstName());
+        System.out.println("Last Name: " + Jef.getLastName());
+        System.out.println("Salary: " + Jef.getSalary());
+        Jef.setSalary(100000);
+        System.out.println("Annual Salary: " + Jef.getAnnualSalary());
+        System.out.println("Raise Salary: "+ Jef.raiseSalary(10));
+        System.out.println(Jef);
+    }
+
 }
