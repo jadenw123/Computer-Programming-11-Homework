bc11afc985f4e59b74ae643bf92be50f6af22a0c
Jaden Wu <804876@learn.vsb.bc.ca>
2021-03-09 10:26:48 -0800

    Class Practice

--- Class Practice/src/courses.java
+++ Class Practice/src/courses.java
@@ -1,2 +1,11 @@
 public class courses {
+    String subjectName;
+    String studentGrade;
+
+    public courses(String subjectName, String studentGrade){
+        this.subjectName = subjectName;
+        this.studentGrade = studentGrade;
+    }
+
+
 }
--- Class Practice/src/student.java
+++ Class Practice/src/student.java
@@ -1,7 +1,35 @@
-public class student {
-    String firstName;
-    String studentId;
+import java.util.ArrayList;
 
-    int[] integers = new int[8];
+public class student {
+    private String firstName;
+    private static int id = 0;
+    private int studentId;
+    private int average = 0;
+    private int add = 0;
+
+
+
+    ArrayList<courses> courseList = new ArrayList<>();
+
+    public student(String firstName){
+        this.firstName = firstName;
+        this.studentId = id;
+        id++;
+
+    }
+
+    public void addCourseToArray(String subjectName, String studentGrade){
+        courses course = new courses(subjectName, studentGrade);
+        courseList.add(course);
+
+    }
+
+    public void averageStudentGrade(average = add/8){
+
+    }
+
+
+
+
 
 }
