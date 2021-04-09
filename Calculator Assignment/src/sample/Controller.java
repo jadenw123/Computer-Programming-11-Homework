55e210637384870258813ca20cf26f0b0f1164b8
Jaden Wu <804876@learn.vsb.bc.ca>
2021-04-09 10:23:39 -0700

    Calculator Assignment

--- Calculator Assignment/src/sample/Controller.java
+++ Calculator Assignment/src/sample/Controller.java
@@ -20,61 +20,68 @@ public class Controller {
         labelDisplay.setText(display);
     }
 
-    ArrayList<Integer> numberList = new ArrayList<>();
+    ArrayList<String> numberList = new ArrayList<>();
     ArrayList<String> operatorList = new ArrayList<>();
 
 
     public void button1(ActionEvent actionEvent) {
         addNumber("1");
-        numberList.add(1);
+        numberList.add("1");
     }
 
     public void button2(ActionEvent actionEvent) {
         addNumber("2");
-        numberList.add(2);
+        numberList.add("2");
     }
 
     public void button3(ActionEvent actionEvent) {
         addNumber("3");
-        numberList.add(3);
+        numberList.add("3");
     }
 
     public void button4(ActionEvent actionEvent) {
         addNumber("4");
-        numberList.add(4);
+        numberList.add("4");
     }
 
     public void button5(ActionEvent actionEvent) {
         addNumber("5");
-        numberList.add(5);
+        numberList.add("5");
     }
 
     public void button6(ActionEvent actionEvent) {
         addNumber("6");
-        numberList.add(6);
+        numberList.add("6");
     }
 
     public void button7(ActionEvent actionEvent) {
         addNumber("7");
-        numberList.add(7);
+        numberList.add("7");
     }
 
     public void button8(ActionEvent actionEvent) {
         addNumber("8");
-        numberList.add(8);
+        numberList.add("8");
     }
 
     public void button9(ActionEvent actionEvent) {
         addNumber("9");
-        numberList.add(9);
+        numberList.add("9");
     }
 
     public void button0(ActionEvent actionEvent) {
         addNumber("0");
-        numberList.add(0);
+        numberList.add("0");
     }
 
     public void buttonEquals(ActionEvent actionEvent){
+        if(numberList.isEmpty() && operatorList.isEmpty()){
+        }
+        else if (numberList.size()>1){
+
+        }
+        else if (operatorList.size()>1){
+        }
 
     }
 
@@ -97,7 +104,6 @@ public class Controller {
             numberList.clear();
             operatorList.clear();
             labelDisplay.setText("0");
-            addOperator("0");
-
     }
+
 }
